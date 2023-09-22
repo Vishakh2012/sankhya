3dbcf7940883ea93ed1ef3e698a8a79c6111d5d3
ebinjose02 <ebin371@gmail.com>
2023-09-23 00:55:04 +0530

    slight changes in speechrecognition

--- react/my-app/src/components/SpeechRecognitionComponent.js
+++ react/my-app/src/components/SpeechRecognitionComponent.js
@@ -1,42 +1,28 @@
 import React from 'react';
-import { useState } from 'react';
-import '../App.css'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
+import styles from './Records.module.css'
 const SpeechRecognitionComponent = () => {
   const { transcript, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
-
+  // const handleSubmit = async(e)=>{
+  //   e.preventDefault();
+  // }
   if (!browserSupportsSpeechRecognition) {
     return <div>Speech recognition is not supported by your browser</div>;
   }
-  const [listening, setListening] = useState(false);
-
-  const startListening = () => {
-    setListening(true);
-  };
-
-  const stopListening = () => {
-    setListening(false);
-  };
-  const start =() =>{
-    SpeechRecognition.startListening();
-    startListening();
-    
-  }
-
-  const stop =() =>{
-    SpeechRecognition.stopListening();
-    stopListening();
-    
-
-  }
 
   return (
-    <div classname='record'>
-      <button onClick={start} style={{ backgroundColor: listening ? 'red' : '' }}disabled={listening} className='button-1'>Start Listening</button>
-      <button onClick={stop} disabled={!listening} className='button-1'>Stop Listening</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <div>Transcript: {transcript}</div>
+    <div className={styles.records}>
+    <div className={styles.buttonset}>
+      <button className={styles.btn1} onClick={SpeechRecognition.startListening }>Start Listening</button>
+      <button className={styles.btn1} onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+      <button className={styles.btn1} onClick={resetTranscript}>Reset</button>
+      
+    </div>
+    <div className={styles.confirm}>
+      Transcript: {transcript}
+      <button  className={styles.btn2}>Confirm</button>
+      </div>
+    
     </div>
   );
 }
