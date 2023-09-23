99ee54b2de0083ee369d3f7880c59fdfbebbb335
sumesh s <sumeshs0202@gmail.com>
2023-09-23 22:41:24 +0530

    signin and signup

--- react/my-app/src/components/Navbar.js
+++ react/my-app/src/components/Navbar.js
@@ -1,20 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Signup from './signup';
-
+import { context } from "../context/Provider";
+import { useContext } from "react";
 
 function BasicExample() {
+  const ctx = useContext(context);
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Signup/>
         <Navbar.Brand href="/">SANKHYA</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/auth">Log In</Nav.Link>
+            {!ctx.authenticated && <Nav.Link href="/auth">Log In</Nav.Link>}
+            {ctx.authenticated && <Nav.Link>Log Out</Nav.Link>}
+            {ctx.authenticated && (
+              <p style={{ paddingTop: "8px", paddingLeft: "15px" }}>
+                welcome {ctx.user}
+              </p>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ react/my-app/src/context/Provider.jsx
@@ -0,0 +1,65 @@
+import { createContext } from "react";
+import { useState } from "react";
+import jwt_decode from "jwt-decode";
+import jwtDecode from "jwt-decode";
+
+//create and initialize context
+export const context = createContext({
+  accesToken: null,
+  setAccesToken: () => {},
+  user: null,
+  login: () => {},
+  authenticated: false,
+});
+
+//provider component
+const Provider = (props) => {
+  //states
+  const [accesToken, setAccesToken] = useState(() => {
+    localStorage.getItem("authToken")
+      ? localStorage.getItem("authToken")
+      : null;
+  });
+  const [user, setUser] = useState(null);
+  const [isauthenticated, setIsAuthenticated] = useState(false);
+  console.log(user);
+  //function definitions
+  const login = async (e) => {
+    e.preventDefault();
+    //getting data from the user
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    const json = { username, password };
+    //sending request to get get the tokens
+    const response = await fetch("http://localhost:8000/api/token/", {
+      mode: "cors",
+      method: "POST",
+      body: JSON.stringify(json),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      const accesToken = data.access;
+      setAccesToken(data);
+      const decoded_token = jwt_decode(accesToken);
+      const username = decoded_token["username"];
+      setUser(username);
+      setIsAuthenticated(true);
+    } else {
+      console.log("Something went wrong");
+    }
+  };
+  const value = {
+    accesToken,
+    setAccesToken,
+    login,
+    user,
+    authenticated: isauthenticated,
+  };
+  return <context.Provider value={value}>{props.children}</context.Provider>;
+};
+
+export default Provider;
