db189d695fed9c8388c58837cfb0e6e14b4cd285
AadeshPS <aadeshps@gmail.com>
2023-09-23 10:53:55 +0530

    pricing,changes in navbar

--- react/my-app/src/components/Home/Home.js
+++ react/my-app/src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../Picture1.png';
+import logo from '../../Picture2.png';
 import './home.css'
 
 
@@ -7,9 +7,15 @@ const Home = () => {
   return (
     <>
     <div className='fade-in'>
-    <img src={logo}alt='logo' className='image'/>
-    <h2 className='Hh'>Sankhya</h2>
+        <div className='jus-logo'>
+             <img src={logo}alt='logo' className='image'/>
+             <h2 className='Hh'>Sankhya</h2>
+        </div>
+    
     <p className='para'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat officiis magnam tenetur rerum laboriosam earum magni tempora facilis voluptatibus repellendus molestiae provident nostrum eos quidem porro, eius eum perspiciatis cupiditate? </p>
+    </div>
+    <div>
+        
     </div>
     </>
   )
--- react/my-app/src/components/Navbar/Navbar.js
+++ react/my-app/src/components/Navbar/Navbar.js
@@ -1,18 +1,24 @@
+
+import'./navbar.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 
+
 function BasicExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary" style={{fontFamily:'Montserrat, sans-serif'}}>
       <Container>
-        <Navbar.Brand href="/">SANKHYA</Navbar.Brand>
+        <Navbar.Brand href="/" style={{color:'#0d6efd'}}>SANKHYA</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/auth">Log In</Nav.Link>
+            <Nav.Link href="/pricing">Pricing</Nav.Link>
+            <Nav.Link href="/analytics">Analytics</Nav.Link>
+          </Nav>
+          <Nav className="ml-auto"> 
+            <Nav.Link href="/auth">Sign In</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
